fced2a9fc66757ba0be096c60210b83c70662dd2
Filipe Silva <filipe.m.silva98@gmail.com>
2024-03-12 20:25:01 +0000

    Added new feature to main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ print("Hello GIT!")
 print("This is a new commit!")
 print("Yet another update!!")
 print("I promise this is the last update!")
+
+def hello():
+    print("This function is to print hello Rebase!")
+
+hello()
+
